a37c6ebd497f55311e5ef99ad550ed196e9518b3
sunyuqiao0609 <sunyuqiao0609@gmail.com>
2018-02-03 15:04:17 -0500

    Add files via upload

---
+++ find the light/find_light_code.ino
@@ -0,0 +1,15 @@
+int sensorPin = A0; // select input for LDR
+int sensorValue = 0; // variable to store the value coming from the sensor
+
+
+void setup() {
+  Serial.begin(9600); // set serial port for communication
+
+}
+
+void loop() {
+  sesorValue = analogRead(sensorPin); // read value from sensor
+  Serial.println(sensorValue); // prints the values coming from the sensor on the screen
+  delay(100);
+}
+
